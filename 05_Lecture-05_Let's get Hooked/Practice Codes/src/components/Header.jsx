7d90d8e77786f8685b003624860c3d6f1f829867
Shoaib Akhtar <shoaibahktar56@gmail.com>
2024-01-25 02:07:03 +0530

    Lecture-05 Added (hooks)
    Episode-05 Let's get Hooked code files added (added a button feature to real time food api project using hooks)

---
+++ 05_Lecture-05_Let's get Hooked/Practice Codes/src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+const Header = () => {
+    return(
+        <div className="nav-container">
+               <img className="nav-logo" src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fast-food-restaurant-logo%2C-restaurant-logo-design-template-33255790cb8e1186b28609dd9afd4ee6_screen.jpg?ts=1668794604" alt="" />
+                 <ul className='nav-items'>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Cart</a>
+                        </li>
+                </ul>
+        </div>
+    )
+}
+export default Header;
---
+++ 05_Lecture-05_Let's get Hooked/Practice Codes/src/constants.jsx
@@ -0,0 +1,364 @@
+
+export const API_DATA_LIST = [
+    {
+    "info": {
+    "id": "673903",
+    "name": "Goila Butter Chicken",
+    "cloudinaryImageId": "5e19832da032dd69547565e27104706f",
+    "costForTwo": "₹600 for two",
+    "cuisines": [
+    "North Indian",
+    "Biryani",
+    "Kebabs",
+    "Desserts"
+    ],
+    "avgRating": 4,
+    "sla": {
+    "deliveryTime": 48,
+    },
+}
+     },
+
+   {
+    "info": {
+    "id": "326637",
+    "name": "Khadak Singh Da Dhaba",
+    "cloudinaryImageId": "8144bf37947c161953f656308d1bfc30",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "North Indian",
+    "Mughlai",
+    "Tandoor",
+    "Indian",
+    "Snacks"
+    ],
+    "avgRating": 4.1,
+    "sla": {
+    "deliveryTime": 35,
+    },
+}
+   },
+
+   {
+    "info": {
+    "id": "223411",
+    "name": "Burger King",
+    "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Burgers",
+    "American"
+    ],
+    "avgRating": 4.2,
+    "sla": {
+    "deliveryTime": 31,
+    },
+}
+   },
+
+   {
+    "info": {
+    "id": "19062",
+    "name": "Halal Pizza Fun",
+    "cloudinaryImageId": "iblt3nwjvtqst643yxan",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Pizzas",
+    "Italian",
+    "Fast Food"
+    ],
+    "avgRating": 4,
+    "sla": {
+    "deliveryTime": 35,
+    },
+}
+   },
+
+   {
+    "info": {
+    "id": "346107",
+    "name": "Al-Nihar",
+    "cloudinaryImageId": "caolf0euwfoui4ha2sjz",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "American",
+    "Burgers",
+    "Snacks"
+    ],
+    "avgRating": 4.6,
+  
+    "sla": {
+    "deliveryTime": 39,
+    },
+    }
+},
+
+{
+    "info": {
+    "id": "365826",
+    "name": "NIC Ice Creams",
+    "cloudinaryImageId": "85825a6d74b1059a63a9b688de9f67ce",
+    "locality": "Shaheen Bagh",
+    "areaName": "Jasola",
+    "costForTwo": "₹120 for two",
+    "cuisines": [
+    "Ice Cream",
+    "Desserts"
+    ],
+    "avgRating": 4.6,
+    "sla": {
+    "deliveryTime": 20,
+    }
+}
+},
+
+{
+    "info": {
+    "id": "422949",
+    "name": "AL-Afzal Nihari",
+    "cloudinaryImageId": "fazmiwhr3ueyvsv6qo71",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Indian"
+    ],
+    "avgRating": 4.4,
+    "sla": {
+    "deliveryTime": 36,
+    },
+    }
+},
+
+{
+    "info": {
+    "id": "394288",
+    "name": "Ballimaran Nahari Restaurant",
+    "cloudinaryImageId": "lniomcs0ke4fim9mfo18",
+    "costForTwo": "₹170 for two",
+    "cuisines": [
+    "Indian",
+    "Biryani"
+    ],
+    "avgRating": 3.8,
+    "sla": {
+    "deliveryTime": 40,
+    },
+    }
+},
+
+{
+    "info": {
+    "id": "37455",
+    "name": "Swaad Restaurant",
+    "cloudinaryImageId": "cba56df320ad5542c5ccfb712ceb1618",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "North Indian",
+    "Mughlai",
+    "Chinese"
+    ],
+    "avgRating": 4.3,
+    "sla": {
+    "deliveryTime": 38,
+    },
+
+    }
+},
+
+{
+    "info": {
+    "id": "817599",
+    "name": "Zahra Restaurant & Cafe",
+    "cloudinaryImageId": "16400d2141cb4bc16ac6b79edfb89298",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Indian",
+    "Tandoor",
+    "Kebabs",
+    "Biryani",
+    "Beverages"
+    ],
+    "avgRating": 4.7,
+    "sla": {
+    "deliveryTime": 28,
+    },
+
+    "info": {
+    "id": "137943",
+    "name": "Pahalwan Ji",
+    "cloudinaryImageId": "ksj3otlt572daqfy1sro",
+    "locality": "Jamia Nagar",
+    "areaName": "Zakir Nagar",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Sweets",
+    "Indian",
+    "Desserts"
+    ],
+    "avgRating": 4.2,
+    "veg": true,
+    "parentId": "15375",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 35,
+    "lastMileTravel": 3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "35 mins",
+    "lastMileTravelString": "3.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2024-01-23 08:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    }
+    ]
+    },
+    "textExtendedBadges": {},
+    "textBased": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹120 OFF",
+    "subHeader": "ABOVE ₹199",
+    "discountTag": "FLAT DEAL"
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {},
+    "cta": {
+    "link": "swiggy://menu?restaurant_id=137943",
+    "text": "RESTAURANT_MENU",
+    "type": "DEEPLINK"
+    }
+    }
+},
+
+{
+    "info": {
+    "id": "49244",
+    "name": "Oven Story Pizza - Standout Toppings",
+    "cloudinaryImageId": "b39e4312ae2e9f65c3b58faaed83f488",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Pizzas",
+    "Pastas",
+    "Italian",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.3,
+    "sla": {
+    "deliveryTime": 30,
+    },
+}
+},
+
+{
+    "info": {
+    "id": "37461",
+    "name": "Qasmi Biryani Centre",
+    "cloudinaryImageId": "c3fufp78dd0ksxbdteho",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "Biryani",
+    "Mughlai",
+    "Indian",
+    "Arabian",
+    "Italian"
+    ],
+    "avgRating": 3.8,
+    "sla": {
+    "deliveryTime": 44,
+    },
+    },
+},
+
+{
+        "info": {
+        "id": "342623",
+        "name": "Nathu's Sweets",
+        "cloudinaryImageId": "p3dubgiqx9uuknw7nkrq",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+        "Desserts",
+        "South Indian",
+        "Chinese"
+        ],
+        "avgRating": 4.1,
+        "sla": {
+        "deliveryTime": 32,
+        },
+    },
+},
+
+{
+    "info": {
+    "id": "36913",
+    "name": "Nazeer Foods",
+    "cloudinaryImageId": "6aa38b251b1ee1662961a6dc19e41e95",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "North Indian",
+    "Biryani",
+    "Mughlai",
+    "Snacks"
+    ],
+    "avgRating": 3.9,
+    "sla": {
+    "deliveryTime": 26,
+    },
+    },
+},
+
+{
+    "info": {
+    "id": "19041",
+    "name": "Karim's",
+    "cloudinaryImageId": "qli8u3t0wahi48ih9fjs",
+    "costForTwo": "₹800 for two",
+    "cuisines": [
+    "Mughlai",
+    "North Indian"
+    ],
+    "avgRating": 3.8,
+    "parentId": "1674",
+    "avgRatingString": "3.8",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 31,
+    },
+    }
+}
+
+]
+
